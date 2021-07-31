d264a79f60d6f23f556a02f61b6360832b6883dc
eylens <eymasac@gmail.com>
2021-07-31 08:03:19 +0700

    display adjusted

--- 0x00-math_complex/0-display.c
+++ 0x00-math_complex/0-display.c
@@ -10,14 +10,15 @@ void display_complex_number(complex c)
 {
 	if (c.im == 0) /* num im = 0 */
 		printf("%f\n", c.re);
+	else if (c.re == 0) /* Only print imagynary number*/
+		printf("%fi\n", c.im);
 	else if (c.im == -1) /*print only im*/
 		printf("%f - i\n", c.re);
 	else if (c.im < -1) /* imaginary is a negative number */
-		printf("%f - %fi\n", c.re, -c.im); /* -c.im x -1 = c.im */
+		c.im = c.im * -1;
+		printf("%f - %fi\n", c.re, c.im);
 	else if (c.im == 1)
 		printf("%f + i\n", c.re);
-	else if (c.re == 0) /* Only print imagynary number*/
-		printf("%fi\n", c.im);
 	else
 		printf("%f + %fi\n", c.re, c.im);
 }
