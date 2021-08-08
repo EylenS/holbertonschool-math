d58b6f96813eaaff15b5bf9c1386665b5e6d8f4f
eylens <eymasac@gmail.com>
2021-08-07 21:42:01 -0500

    division

---
+++ 0x00-math_complex/7-division.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * multiplication - Performs the mult. between complex numbers.
+ * @c1: First complex number.
+ * @c2: Second complex number.
+ * @c3: Complex number after the multiplication.
+ */
+void division(complex c1, complex c2, complex *c3)
+{
+	double a, b, d;
+
+	d = (c2.re * c2.re) + (c2.im * c2.im); /* denominator */
+	a = ((c1.re * c2.re) + (c1.im * c2.im)) / d; /* real part */
+	b = ((c1.im * c2.re) - (c1.re * c2.im)) / d; /* imaginary part */
+	c3->re = a;
+	c3->im = b;
+}
